12084781545a9abcff561a58919d5e08495789a3
andrew <wertuk2@gmail.com>
2015-06-30 21:10:54 +0300

    ticket1: add log info

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -10,6 +10,6 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
         context['data'] = About_me.objects.get(pk=1)
-        logger.info(self.request)
-        logger.debug(context)
+        logger.info(u'Landing page request %s', self.request)
+        logger.debug(u'Landing page context %s', context)
         return context
